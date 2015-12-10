e72a5db56a0b14d5c1f627f9fd3915e37ead4fac
Joakim <joakim.koch@mecglobal.com>
2015-12-10 10:55:03 +0100

    added new function for get API

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.resrobot.se/trip.json?key=0ed22fec-ce16-41b2-b7c7-3e831ef008bc&originId=7400001&destId=7400002", false);
+var url = "https://api.resrobot.se/trip.xml?key=0ed22fec-ce16-41b2-b7c7-3e831ef008bc&originId=7400001&destId=7400002";
 
-console.log(xhr.statusText);
+var url2 = "https://api.resrobot.se/trip?originId=7400001&destId=7400003&format=json&key=0ed22fec-ce16-41b2-b7c7-3e831ef008bc"
+
+var xmlHttp = new XMLHttpRequest();
+
+xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        var myArr = JSON.parse(xmlHttp.responseText);
+        myFunction(myArr);
+    }
+};
+
+xmlHttp.open("GET", url, true);
+xmlHttp.send();
+
+console.log(xmlHttp);
+
